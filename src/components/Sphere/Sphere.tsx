be4989bdc491926f48ec4dc89f581f67ba2d62fb
PeterTYLiu <peter.ty.liu@gmail.com>
2026-02-02 22:38:44 -0500

    Sphere radius corrected

--- src/components/Sphere/Sphere.tsx
+++ src/components/Sphere/Sphere.tsx
@@ -1,17 +1,16 @@
 import type { CSSProperties, ReactElement } from "react";
 import styles from "./Sphere.module.css";
 
-export default function Sphere({ resolution = 5 }: { id: string; resolution?: number }) {
-  const facesPerLayer = 4 * resolution;
+export default function Sphere({ resolution = 4 }: { id: string; resolution?: number }) {
   const numOfLayers = 2 * resolution;
+  const facesPerLayer = numOfLayers * 2;
 
   const faces: ReactElement[] = [];
-  const faceHeight = 2 * Math.sin(Math.PI / (numOfLayers * 2));
-  const faceWidthFactor = 2 * Math.sin(Math.PI / facesPerLayer);
+  const normalizedFaceSideLength = 2 * Math.sin(Math.PI / facesPerLayer);
 
   for (let layer = 0; layer < numOfLayers; layer++) {
     const xAngle = (layer * 180) / numOfLayers + 90 / numOfLayers;
-    const bottomRadiusAtThisLayer = Math.cos(Math.PI / 2 - Math.PI * ((layer + 1) / numOfLayers));
+    // const bottomRadiusAtThisLayer = Math.cos(Math.PI / 2 - Math.PI * ((layer + 1) / numOfLayers));
     // const topRadiusAtThisLayer = Math.cos(Math.PI / 2 - Math.PI * (layer / numOfLayers));
     for (let face = 0; face < facesPerLayer; face++) {
       const zAngle = (face * 360) / facesPerLayer;
@@ -19,13 +18,11 @@ export default function Sphere({ resolution = 5 }: { id: string; resolution?: nu
         <div
           className={styles.face}
           key={`${face}-${layer}`}
-          data-id={bottomRadiusAtThisLayer}
           style={
             {
               "--layers": numOfLayers,
-              width: `calc(var(--radius) * ${faceWidthFactor})`,
-              transform: `rotateZ(${zAngle}deg) rotateX(${xAngle}deg)`,
-              height: `calc(var(--radius) * ${faceHeight})`,
+              "--normalized-face-side-length": normalizedFaceSideLength,
+              transform: `translateY(-50%) translateX(-50%) rotateZ(${zAngle}deg) rotateX(${xAngle}deg)`,
             } as CSSProperties
           }
         />,
