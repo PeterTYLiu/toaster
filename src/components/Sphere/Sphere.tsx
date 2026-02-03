27f767dd4324ff936ddcf43eb1acb1a3e9c481f8
PeterTYLiu <peter.ty.liu@gmail.com>
2026-02-02 20:10:27 -0500

    Improve sphere

--- src/components/Sphere/Sphere.tsx
+++ src/components/Sphere/Sphere.tsx
@@ -1,47 +1,53 @@
-import type { CSSProperties, ReactNode } from "react";
-import styles from "./Sphere.module.scss";
+import type { CSSProperties, ReactElement } from "react";
+import styles from "./Sphere.module.css";
 
-export default function Sphere({
-  numPlanes = 12,
-  numSpokes = 16,
-  id,
-}: {
-  id: string;
-  numPlanes?: number;
-  numSpokes?: number;
-}) {
-  const planes: ReactNode[] = [];
-  for (let i = 0; i < numPlanes; i++) {
-    const spokes: ReactNode[] = [];
+export default function Sphere({ resolution = 5 }: { id: string; resolution?: number }) {
+  const facesPerLayer = 4 * resolution;
+  const numOfLayers = 2 * resolution;
 
-    for (let j = 0; j < numSpokes; j++) {
-      spokes.push(
+  const faces: ReactElement[] = [];
+  const faceHeight = 2 * Math.sin(Math.PI / (numOfLayers * 2));
+  const faceWidthFactor = 2 * Math.sin(Math.PI / facesPerLayer);
+
+  for (let layer = 0; layer < numOfLayers; layer++) {
+    const xAngle = (layer * 180) / numOfLayers + 90 / numOfLayers;
+    const bottomRadiusAtThisLayer = Math.cos(Math.PI / 2 - Math.PI * ((layer + 1) / numOfLayers));
+    // const topRadiusAtThisLayer = Math.cos(Math.PI / 2 - Math.PI * (layer / numOfLayers));
+    for (let face = 0; face < facesPerLayer; face++) {
+      const zAngle = (face * 360) / facesPerLayer;
+      faces.push(
         <div
-          key={`${id}-${i}-${j}`}
-          className={styles.spoke}
-          style={{ "--spoke": j } as CSSProperties}
-        >
-          <div className={styles.face} />
-        </div>
+          className={styles.face}
+          key={`${face}-${layer}`}
+          data-id={bottomRadiusAtThisLayer}
+          style={
+            {
+              "--layers": numOfLayers,
+              width: `calc(var(--radius) * ${faceWidthFactor})`,
+              transform: `rotateZ(${zAngle}deg) rotateX(${xAngle}deg)`,
+              height: `calc(var(--radius) * ${faceHeight})`,
+            } as CSSProperties
+          }
+        />,
+        // Make this work in the future
+        // <svg
+        //   className={styles.face}
+        //   key={`${face}-${layer}`}
+        //   data-id={bottomRadiusAtThisLayer}
+        //   style={
+        //     {
+        //       "--layers": numOfLayers,
+        //       width: `calc(var(--radius) * ${faceWidthFactor})`,
+        //       transform: `rotateZ(${zAngle}deg) rotateX(${xAngle}deg)`,
+        //       height: `calc(var(--radius) * ${faceHeight})`,
+        //     } as CSSProperties
+        //   }
+        // >
+        //   <polygon points={`0,0 1,1 1,1 0`} />
+        // </svg>,
       );
     }
-
-    planes.push(
-      <div
-        key={`${id}-${i}`}
-        className={styles.plane}
-        style={
-          {
-            "--plane": i,
-            "--planes": numPlanes,
-            "--spokes": numSpokes,
-          } as CSSProperties
-        }
-      >
-        {spokes}
-      </div>
-    );
   }
 
-  return <>{planes}</>;
+  return <>{faces}</>;
 }
